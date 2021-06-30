2c7b230999cc450496c80276618bfd22efe9948d
vpn <vpn@NBBV030975.local>
2021-06-30 18:02:52 -0300

    tableView

---
+++ MovieDB/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  MovieDB
+//
+//  Created by vpn on 30/06/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
